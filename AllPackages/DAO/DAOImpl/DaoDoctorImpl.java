302b8722d15d292d9156173c5ca69caf5803414b
practical <er@mycommit.com>
2023-10-04 12:01:18 +0600

    inital commit

--- AllPackages/DAO/DAOImpl/DaoDoctorImpl.java
+++ AllPackages/DAO/DAOImpl/DaoDoctorImpl.java
@@ -7,4 +7,23 @@ import java.util.List;
 
 public class DaoDoctorImpl implements DaoDoctorInterface {
 
+    @Override
+    public String addDoctorToHospital(Long id, Doctor doctor) {
+        return null;
+    }
+
+    @Override
+    public Doctor findDoctorById(Long id) {
+        return null;
+    }
+
+    @Override
+    public String updateDoctor(Long id, Doctor doctor) {
+        return null;
+    }
+
+    @Override
+    public void deleteDoctorById(Long id) {
+
+    }
 }
